1c5ce4a03c0e4895e9f8b6c5bf74dd4a2b4a3587
Kenny Dunbar <Kensleyrae2014@gmail.com>
2022-06-04 02:09:21 -0400

    Update accounts.js
    updated function

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -21,30 +21,34 @@ function getTotalNumberOfBorrows(account, books) {
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
-  let result = [];
-  let borrowMatch = [];
-  books.forEach((item) => {
-   const borrowed = item.borrows;
-   const book = {
-    id: item.id,
-    title: item.title,
-    genre: item.genre,
-    authorId: item.authorId,
-    author: {},
-    borrows: {}
-   };
-   const { id, title, genre, authorId, author, borrows } = book;
- 
-   borrowed.forEach((borrow) => {
-    if (borrow.id === account.id && borrow.returned === false) {
-     result.push(book);
-     borrowMatch.push(borrow);
-     book.borrows = borrowMatch;
-     book.author = authors.filter((auth) => auth.id === book.authorId)[0];
-    }
-   });
-  });
-  return result;
+  return possessedByAccount();
+
+  function possessedByAccount() {
+    let result = [];
+    let borrowMatch = [];
+    books.forEach((item) => {
+      const borrowed = item.borrows;
+      const book = {
+        id: item.id,
+        title: item.title,
+        genre: item.genre,
+        authorId: item.authorId,
+        author: {},
+        borrows: {}
+      };
+      const { id, title, genre, authorId, author, borrows } = book;
+
+      borrowed.forEach((borrow) => {
+        if (borrow.id === account.id && borrow.returned === false) {
+          result.push(book);
+          borrowMatch.push(borrow);
+          book.borrows = borrowMatch;
+          book.author = authors.filter((auth) => auth.id === book.authorId)[0];
+        }
+      });
+    });
+    return result;
+  }
 }
 
 module.exports = {
